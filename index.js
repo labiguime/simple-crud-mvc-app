66df583701af14db509fc23a405191c4bddc095f
labiguime <leoabiguime@gmail.com>
2020-03-23 02:44:06 -0700

    Implement delete and add View

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const mysql = require('mysql');
 const path = require("path");
+const User = require('./models/user.js');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 // const connection = mysql.createConnection({
 //   host: "34.68.51.2",
 //   database: "asn4db",
@@ -28,10 +32,12 @@ app.set('view engine', 'ejs');
 
 app.use('/users/', require('./routes/users'));
 
-app.get('/', (req, res) => {
-	return res.render('home', {test: 'name'});//res.send('You have reached the home page.');
+app.get('/', async function (req, res) {
+	const users = await User.findAll();
+	console.log(users);
+	return res.render('home', {users});//res.send('You have reached the home page.');
 });
 
 app.listen(3000, () => {
 	console.log('The server is up and running on port 3000');
-})
+});
